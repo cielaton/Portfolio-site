2816bd1cc8c9b9e217a944cc18ff6939d04caebb
cielaton <vuwin24680@gmail.com>
2024-07-11 21:09:55 +0700

    Fixed the color plate config The color configuration should extend the default values.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<h1 class="text-green-400">Welcome to SvelteKit</h1>
+<h1 class="text-Green">Welcome to SvelteKit</h1>
 <p>
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the
   documentation
