03284709287d50cc31064e9367f3a94f718dd673
cielaton <vuwin24680@gmail.com>
2024-07-12 12:23:36 +0700

    Formed project structure and added some elements to track scroll behavior

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,21 @@
 <script>
   import "../app.css";
+  import Footer from "/src/components/Footer.svelte";
+  import Header from "/src/components/Header.svelte";
+
+  let scrollY;
+  let innerHeight = 0,
+    innerWidth = 0;
+
+  const goTop = () => {
+    document.body.scrollIntoView();
+  };
 </script>
 
-<slot />
+<div class="relative flex flex-col w-full min-h-screen">
+  <Header />
+  <slot />
+  <Footer />
+</div>
+
+<svelte:window bind:scrollY bind:innerHeight bind:innerWidth />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
-<h1 class="text-Green">Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the
-  documentation
-</p>
+<script>
+  import Main from "/src/components/Main.svelte"
+</script>
+
+<Main />
