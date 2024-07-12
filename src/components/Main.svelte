d2cb0b8bc0d3cf9b31b1703928d8f32cd5ccec56
cielaton <vuwin24680@gmail.com>
2024-07-12 22:49:09 +0700

    Added introduction contents and minor changes Changed the particles configuration to reduce the number of rendered particles. Set the z-index of the particles background to -1, could not be interated with anymore. Hide the navigation buttons on small screen s

---
+++ src/components/Main.svelte
@@ -0,0 +1,38 @@
+<script>
+</script>
+
+<main class="flex flex-col px-10 grow">
+  <section
+    id="introduction"
+    class="grid grid-cols-1 lg:grid-cols-2 gap-20 grow"
+  >
+    <div
+      class="flex flex-col gap-5 text-center lg:justify-center lg:text-left mt-10 lg:mt-0"
+    >
+      <h1 class="text-4xl sm:text-5xl md:text-6xl font-bold">
+        Hi! I'm Vo Tuan <span class="text-Teal">Kiet</span>
+      </h1>
+      <p class="text-base sm:text-lg md:text-xl">
+        I am a <span class="text-Teal">Software Developer</span> who is trying to
+        achieve an Engineering degree in Embedded Systems and IoT, along with being
+        a GNU/Linux enthusiast.
+      </p>
+      <div class="flex mr-auto ml-auto lg:mr-auto lg:ml-0">
+        <a
+          href="https://www.fiverr.com/kisto1073"
+          target="_blank"
+          class="relative flex overflow-hidden px-6 py-3 group rounded-lg bg-white text-Base text-base sm:text-lg md:text-xl"
+        >
+          <h4 class="relative text-Base font-medium z-[1]">Fiverr's gigs</h4>
+          <i class="ri-external-link-fill font-medium z-[1] ml-2"> </i>
+          <div
+            class="absolute top-0 right-full w-full h-full bg-Text group-hover:translate-x-full z-0 duration-200"
+          ></div>
+        </a>
+        <div class="flex-none mr-auto bg-Red">
+          <i class="ri-arrow-down-double-fill text-white z-[1]"></i>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
