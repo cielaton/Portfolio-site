f68f82325853b2c44c9b567fd346279f26f4371b
cielaton <vuwin24680@gmail.com>
2024-07-14 21:57:26 +0700

    Added Tools page data via object model and corresponding icon assets Changed particles-js folder name 50#	new file:   static/assets/Main/Tools/Linux-administration/Debian.png

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,16 +1,9 @@
 <script>
   import Introduction from "./Main/Introduction.svelte";
+  import Tools from "./Main/Tools.svelte";
 </script>
 
 <main class="flex flex-col px-5 md:px-10 min-h-svh">
   <Introduction />
-
-  <section id="Tools" class="flex flex-col px-4 lg:px-20 min-h-screen">
-    <div class="flex gap-5 items-center">
-      <div class="bg-Teal rounded-lg relative leading-none p-2 text-Base text-2xl">
-        <i class="ri-settings-5-fill"></i>
-      </div>
-      <h2 class="font-semibold text-3xl md:text-4xl">Favourite development tools</h2>
-    </div>
-  </section>
+  <Tools />
 </main>
---
+++ src/components/Main/Tools.svelte
@@ -0,0 +1,25 @@
+<script>
+</script>
+
+<section
+  id="Tools"
+  class="flex flex-col px-4 lg:px-20 pt-5 lg:pt-20 gap-5 md:gap-10"
+>
+  <div id="Heading" class="flex gap-5 items-center">
+    <div
+      class="bg-Teal rounded-lg relative leading-none p-2 text-Base text-3xl"
+    >
+      <i class="ri-settings-5-fill"></i>
+    </div>
+    <h2 class="font-semibold text-3xl md:text-5xl">
+      Favourite development tools
+    </h2>
+  </div>
+
+  <div id="Body" class="grid grid-cols-8 gap-5">
+    <div class="col-span-5 bg-Base rounded-lg">
+      <p>ads</p>
+    </div>
+  </div>
+  <img class="aspect-auto w-80" src="assets/Main/Tools/Front-end/HTML.png" alt="ehe">
+</section>
