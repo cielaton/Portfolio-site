e48ef5834ce29886fcb4368b0243c2cbefac7dca
cielaton <vuwin24680@gmail.com>
2024-07-19 00:27:15 +0700

    Better style for the Tab component

--- src/components/Main.components/Projects.svelte
+++ src/components/Main.components/Projects.svelte
@@ -10,18 +10,18 @@
     };
 </script>
 
-<section id="Projects" class="flex flex-col gap-10 pt-10 lg:pt-40">
+<section class="flex flex-col gap-10 pt-10 lg:pt-40" id="Projects">
     <h2
-            id="Heading"
             class="text-center font-semibold text-2xl md:text-4xl lg:text-5xl"
+            id="Heading"
     >
         Curious to see my <span class="text-Teal">creation</span>?
     </h2>
-    <Tab.Root id="Body" class="">
-        <Tab.Head class="mx-auto w-1/3 flex flex-row gap-4 px-2 py-2 bg-Base rounded-lg">
+    <Tab.Root class="" id="Body">
+        <Tab.Head class="mx-auto w-1/3 flex flex-row gap-4 px-2 py-2 bg-Surface0 rounded-lg">
             {#each tabs as tabName, index}
                 <Tab.HeadItem
-                        class="w-full flex gap-3 py-2 justify-center"
+                        class={"w-full flex gap-3 py-2 justify-center rounded-lg " + (activeTabId === index ? "bg-Mantle text-Teal" : "")}
                         id={index}
                         on:click={() => handleClick(index)}>
                     {#if tabName === "Personal work"}
@@ -34,7 +34,7 @@
                 >
             {/each}
         </Tab.Head>
-        <Tab.Content class="" id={0} {activeTabId}>Personal Work</Tab.Content>
-        <Tab.Content class="" id={1} {activeTabId}>Team projects</Tab.Content>
+        <Tab.Content {activeTabId} class="" id={0}>Personal Work</Tab.Content>
+        <Tab.Content {activeTabId} class="" id={1}>Team projects</Tab.Content>
     </Tab.Root>
 </section>
